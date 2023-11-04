9ba6109794f7ab55584a007de17f7630411065a9
arshiKhatoon <arshikhatoon62000@gmail.com>
2023-11-04 19:42:00 +0530

    Intial files

---
+++ stringcount.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int getcharcount(char name[]){
+
+    int count = 0;
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+int main()
+{
+    char name[20];
+    cout<<"enter your name ";
+    cin>>name;
+    cout<<"length of string  "<<getcharcount(name);
+
+}
